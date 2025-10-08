4f37c108f0f0f7e523654a17f2c923a954725d8c
たすくん <nizi.minato@gmail.com>
2025-10-08 16:52:28 +0900

    [fix]: エラーページ処理の修正

--- routes/_error.tsx
+++ routes/_error.tsx
@@ -13,27 +13,55 @@ export default define.page(function ErrorPage(props: PageProps) {
 
     if (error instanceof HttpError) {
         const status = error.status;
-        return (
-            <>
-                <Head>
-                    <title>Oh no...</title>
-                </Head>
 
-                <div>
-                    <Header location="/" />
-                    <main className="min-h-screen pt-16">
-                        <div className="mt-16 text-center">
-                            <h1 className="mt-32 text-3xl">
-                                Error: {status}
-                            </h1>
-                            <HistoryBack>前のページに戻る</HistoryBack>
-                        </div>
-                    </main>
-                    <Footer />
-                </div>
-            </>
-        );
+        if (status === 404) {
+            return (
+                <>
+                    <Head>
+                        <title>お探しのページは見つかりませんでした。</title>
+                    </Head>
+
+                    <div>
+                        <Header location="/" />
+                        <main className="min-h-screen pt-16">
+                            <div className="mt-16 text-center">
+                                <h1 className="mt-32 text-3xl">
+                                    404 Page not Found
+                                </h1>
+                                <p>
+                                    お探しのページは見つかりませんでした。
+                                </p>
+                                <HistoryBack>前のページに戻る</HistoryBack>
+                            </div>
+                        </main>
+                        <Footer />
+                    </div>
+                </>
+            );
+        }
     }
 
-    return <></>;
+    return (
+        <>
+            <Head>
+                <title>Something went wron.</title>
+            </Head>
+
+            <div>
+                <Header location="/" />
+                <main className="min-h-screen pt-16">
+                    <div className="mt-16 text-center">
+                        <h1 className="mt-32 text-3xl">
+                            Something went wrong.
+                        </h1>
+                        <p>
+                            ページで問題が発生しました。
+                        </p>
+                        <HistoryBack>前のページに戻る</HistoryBack>
+                    </div>
+                </main>
+                <Footer />
+            </div>
+        </>
+    );
 });
