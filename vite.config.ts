fc742103f949869d5e5951b6b25b27f984352570
たすくん <nizi.minato@gmail.com>
2025-10-08 20:52:51 +0900

    [add]: prismjsの追加

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import { fresh } from "@fresh/plugin-vite";
 import tailwindcss from "@tailwindcss/vite";
 
+import "prismjs";
+
 export default defineConfig({
   plugins: [fresh(), tailwindcss()],
 });
