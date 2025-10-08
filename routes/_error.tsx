887f2898a5830f779371e4c16ba7e2295f696a14
たすくん <nizi.minato@gmail.com>
2025-10-08 16:22:10 +0900

    [fix]: _errorでエラーコードの表示修正

--- routes/_error.tsx
+++ routes/_error.tsx
@@ -36,30 +36,31 @@ export default define.page(function ErrorPage(props: PageProps) {
                     </div>
                 </>
             );
+        } else {
+            return (
+                <>
+                    <Head>
+                        <title>Oh no...</title>
+                    </Head>
+
+                    <div>
+                        <Header location="/" />
+                        <main className="min-h-screen pt-16">
+                            <div className="mt-16 text-center">
+                                <h1 className="mt-32 text-3xl">
+                                    {status} Page Not Found
+                                </h1>
+                                <p>
+                                    お探しのページは見つかりませんでした。
+                                </p>
+                                <HistoryBack>前のページに戻る</HistoryBack>
+                            </div>
+                        </main>
+                        <Footer />
+                    </div>
+                </>
+            );
         }
     }
-
-    return (
-        <>
-            <Head>
-                <title>Oh no...</title>
-            </Head>
-
-            <div>
-                <Header location="/" />
-                <main className="min-h-screen pt-16">
-                    <div className="mt-16 text-center">
-                        <h1 className="mt-32 text-3xl">
-                            404 Page Not Found
-                        </h1>
-                        <p>
-                            お探しのページは見つかりませんでした。
-                        </p>
-                        <HistoryBack>前のページに戻る</HistoryBack>
-                    </div>
-                </main>
-                <Footer />
-            </div>
-        </>
-    );
+    return <></>;
 });
