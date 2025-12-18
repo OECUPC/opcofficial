2716d017545ae392ea47ca958a888da8d325b6d5
たすくん <nizi.minato@gmail.com>
2025-12-18 19:41:39 +0900

    [fix]: github-slugger(jsr:@deno:gfm/mod.ts内)をビルド対象から除外

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import { fresh } from "@fresh/plugin-vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [fresh(), tailwindcss()],
+    plugins: [fresh(), tailwindcss()],
+    build: {
+        rollupOptions: {
+            external: [
+                // github-slugger(jsr:@deno:gfm/mod.ts内)をビルド対象から除外
+                /^npm:github-slugger/,
+            ],
+        },
+    },
 });
