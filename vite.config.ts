4cb4bf652e651c4b0a6fae653aa1c0f417c8bc7d
たすくん <nizi.minato@gmail.com>
2025-12-18 23:00:42 +0900

    [update]: freshのアップデート

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,4 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [fresh(), tailwindcss()],
-    build: {
-        rollupOptions: {
-            external: [
-                // npm:をビルド対象から除外(jsr:@deno/gfmでエラーが出るため)
-                /^npm:/,
-            ],
-        },
-    },
 });
