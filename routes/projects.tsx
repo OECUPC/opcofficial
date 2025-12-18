da1a49827031de0e70f8c809c3aca36f8019680e
tas9n <hw22a102@oecu.jp>
2025-12-18 18:19:02 +0900

    [update]: microCMSを利用せず、assets/projects/以下にプロジェクト.mdを用意する準備

--- routes/projects.tsx
+++ routes/projects.tsx
@@ -3,17 +3,19 @@ import { PageProps } from "fresh";
 import { Header } from "../islands/Header.tsx";
 import { Footer } from "../components/Footer.tsx";
 
-import RequestMicroCMSAPI from "../tools/RequestMicroCMSAPI.ts";
-
-import { ProjectItem } from "../types/ProjectItem.ts";
 import { define } from "../utils.ts";
 
-interface Data {
-    projects: ProjectItem[];
-}
+import { walk } from "jsr:@std/fs@^1.0.19/walk";
 
 export const handler = define.handlers({
     async GET(_) {
+        for await (const entry of walk("./assets/projects")) {
+            if (!entry.isFile) continue;
+
+            const file = await Deno.readTextFile(entry.path);
+        }
+
+        /*
         const json = await RequestMicroCMSAPI("projects", false);
 
         const rawData = json["contents"];
@@ -36,10 +38,13 @@ export const handler = define.handlers({
         }
 
         return { data: { projects } };
+        */
+
+        return { data: {} };
     },
 });
 
-export default function Home({ data }: PageProps<Data>) {
+export default function Home({ data }: PageProps) {
     return (
         <div>
             <Header location="/projects" />
@@ -47,39 +52,6 @@ export default function Home({ data }: PageProps<Data>) {
                 <article className="bg-white">
                     <h1 className="text-4xl text-center py-8">プロジェクト</h1>
                     <article className="grid gap-8 grid-cols-1 lg:grid-cols-3 p-8">
-                        {data.projects.map((elem) => (
-                            <section className="w-full rounded-lg transition-shadow duration-300 shadow-sm hover:shadow-lg">
-                                <a
-                                    className="text-center"
-                                    href={`/project/${elem.id}`}
-                                >
-                                    <figure>
-                                        <img
-                                            className="w-full h-56 shadow-sm object-cover object-center"
-                                            src={(0 < elem.eyecatch.url.length)
-                                                ? elem.eyecatch?.url
-                                                : "/images/no_image.webp"}
-                                        />
-                                        <figcaption className="mt-2">
-                                            <h2 className="text-2xl">
-                                                {elem.title}
-                                            </h2>
-                                            <p className="text-gray-400">
-                                                {elem.updated_at.toLocaleString(
-                                                    "ja-JP",
-                                                    {
-                                                        timeZone: "Asia/Tokyo",
-                                                    },
-                                                )}
-                                            </p>
-                                            <p className="whitespace-pre-wrap p-4">
-                                                {elem.description}
-                                            </p>
-                                        </figcaption>
-                                    </figure>
-                                </a>
-                            </section>
-                        ))}
                     </article>
                 </article>
             </main>
