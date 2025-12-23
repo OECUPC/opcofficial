26b19c15c43fee82eceebc5d684c6609b77b2d50
たすくん <nizi.minato@gmail.com>
2025-12-23 22:48:54 +0900

    [update]: エラーページを簡略化

--- routes/_error.tsx
+++ routes/_error.tsx
@@ -1,13 +1,47 @@
-import { define } from "../utils.ts";
-
+import { HttpError, PageProps } from "fresh";
 import { Head } from "fresh/runtime";
+
+import { define } from "../utils.ts";
 
 import { Header } from "../islands/Header.tsx";
 import { Footer } from "../components/Footer.tsx";
 
-import { HttpError, PageProps } from "fresh";
+import { STATUS_TEXT } from "@std/http/status";
 
-export default define.page(function ErrorPage(props: PageProps) {
+import HistoryBack from "../islands/HistoryBack.tsx";
+
+function ErrorPage(props: { code: number; description: string }) {
+    return (
+        <>
+            <Head>
+                <title>{props.description}</title>
+            </Head>
+            <div>
+                <Header location="/" />
+                <main className="min-h-screen pt-16">
+                    <div className="mt-16 text-center">
+                        <h1 className="mt-32 text-3xl">
+                            {/* STATUS_TEXTのkeyがanyなのでエラー抑制のためにas keyof typeof */}
+                            {props.code} {STATUS_TEXT[
+                                props.code as keyof typeof STATUS_TEXT
+                            ]}
+                        </h1>
+                        <p className="m-4">
+                            {props.description}
+                        </p>
+
+                        <HistoryBack>
+                            ひとつ前のページに戻る
+                        </HistoryBack>
+                    </div>
+                </main>
+                <Footer />
+            </div>
+        </>
+    );
+}
+
+export default define.page((props: PageProps) => {
     const error = props.error;
 
     console.log("== Error ==");
@@ -17,51 +51,15 @@ export default define.page(function ErrorPage(props: PageProps) {
         const status = error.status;
 
         if (status === 404) {
-            return (
-                <>
-                    <Head>
-                        <title>お探しのページは見つかりませんでした。</title>
-                    </Head>
-
-                    <div>
-                        <Header location="/" />
-                        <main className="min-h-screen pt-16">
-                            <div className="mt-16 text-center">
-                                <h1 className="mt-32 text-3xl">
-                                    404 Page not Found
-                                </h1>
-                                <p>
-                                    お探しのページは見つかりませんでした。
-                                </p>
-                            </div>
-                        </main>
-                        <Footer />
-                    </div>
-                </>
-            );
+            return ErrorPage({
+                code: 404,
+                description: "お探しのページは見つかりませんでした。",
+            });
         }
     }
 
-    return (
-        <>
-            <Head>
-                <title>Something went wrong.</title>
-            </Head>
-
-            <div>
-                <Header location="/" />
-                <main className="min-h-screen pt-16">
-                    <div className="mt-16 text-center">
-                        <h1 className="mt-32 text-3xl">
-                            Something went wrong.
-                        </h1>
-                        <p>
-                            ページで問題が発生しました。
-                        </p>
-                    </div>
-                </main>
-                <Footer />
-            </div>
-        </>
-    );
+    return ErrorPage({
+        code: 500,
+        description: "問題が発生しました。",
+    });
 });
